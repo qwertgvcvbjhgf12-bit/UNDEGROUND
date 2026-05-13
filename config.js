fdacc29f60fe8d1f7965e481be2d3406f182fb6d
ChunkyCat22 <qwertgvcvbjhgf12@gmail.com>
2026-05-13 15:57:41 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ucbkhvjryunzvvmcvfma.supabase.co/rest/v1/";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjYmtodmpyeXVuenZ2bWN2Zm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTIyODEsImV4cCI6MjA5NDI2ODI4MX0.tq_Ers5cY_ph0qwXrZVse0gkZJi3o64cARE60Gio1Kc";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
